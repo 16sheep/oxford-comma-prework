1410899ee74976f69030755eaa1e9d5b1231f8e8
marjutubli@gmail.com <marjutubli@gmail.com>
2018-05-09 10:25:58 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,4 +1,8 @@
 def oxford_comma(array)
+  if array.length == 1
+    array.join
+  elsif array.length == 2
+    array.join(" and ")
   array.insert(-2, "and")
   string = array.join(", ")
   last_occurance = string.rindex(",")
