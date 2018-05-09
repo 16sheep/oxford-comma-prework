4fbf1c01a4c02d0279db2ccfbec866a85f16a2ff
marjutubli@gmail.com <marjutubli@gmail.com>
2018-05-09 10:21:14 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,7 +1,7 @@
 def oxford_comma(array)
   array.insert(-2, "and")
   string = array.join(", ")
-  last_occurance = string.rindex(", ")
+  last_occurance = string.rindex(",")
   string = string.slice(last_occurance)
   
 end
